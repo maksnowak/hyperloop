c5ece5b9563e8a05afdbed11b3276ab0f8acebde
Duszke333 <duszke333@gmail.com>
2024-04-22 15:59:37 +0200

    Stations added, please run these in the console.

---
+++ data.sql
@@ -0,0 +1,16 @@
+-- stacje
+insert into hyperloop.public.stations values (1, 'Wadowice', 49.883373, 19.493260); --Muzeum JPII
+insert into hyperloop.public.stations values (2, 'Warszawa', 52.232149, 21.006627); --PKiN
+insert into hyperloop.public.stations values (3, 'Kraków', 50.054140, 19.936580); --Wawel
+insert into hyperloop.public.stations values (4, 'Bydgoszcz', 53.121684, 17.998683); --Muzeum mydła i brudu
+insert into hyperloop.public.stations values (5, 'Gdańsk', 54.352025, 18.646638); --Muzeum II Wojny Światowej
+insert into hyperloop.public.stations values (6, 'Poznań', 52.406374, 16.925168); --Stary Rynek
+insert into hyperloop.public.stations values (7, 'Wrocław', 51.107883, 17.038538); --Ostrów Tumski
+insert into hyperloop.public.stations values (8, 'Szczecin', 53.428543, 14.552812); --Zamek Książąt Pomorskich
+insert into hyperloop.public.stations values (9, 'Lublin', 51.246454, 22.568446); --Zamek Lubelski
+insert into hyperloop.public.stations values (10, 'Katowice', 50.264892, 19.023782); --Spodek
+insert into hyperloop.public.stations values (11, 'Łódź', 51.768732, 19.456991); --Manufaktura
+insert into hyperloop.public.stations values (12, 'Białystok', 53.132488, 23.168840); --Pałac Branickich
+insert into hyperloop.public.stations values (13, 'Olsztyn', 53.778422, 20.480119); --Zamek Kapituły Warmińskiej
+insert into hyperloop.public.stations values (14, 'Rzeszów', 50.041187, 21.999121); --Zamek Lubomirskich
+insert into hyperloop.public.stations values (15, 'Kielce', 50.866077, 20.628568); --Pałac Biskupów Krakowskich
