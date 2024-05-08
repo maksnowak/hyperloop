94b04f2b9c54d2f2d05d368af242ff81f7bdb3e5
Alicja <alicjakruszewicz3@gmail.com>
2024-05-08 20:27:13 +0200

    move sidebar component

--- src/app/capsules/page.tsx
+++ src/app/capsules/page.tsx
@@ -10,9 +10,9 @@ const Capsules = async () => {
   const capsules = await prisma.capsules.findMany();
   return (
     <>
-      <div className="text-center">Capsules</div>
-      <Sidebar />
+      <h1 className="text-center bold">Capsules</h1>
       <div className="relative">
+        <Sidebar />
         <div className="ml-64 static grid grid-cols-3 inset-y-0 right-0 gap-y-8">
           {capsules.map((capsule) => {
             return <Capsule key={capsule.capsule_id} {...capsule} />;
