39e107e0bd98406a71c8f28438ed989a10c17f54
Alicja <alicjakruszewicz3@gmail.com>
2024-05-08 14:26:13 +0200

    Add base website skeleton

---
+++ src/app/map/page.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "@/components/sidebar";
+import React from "react";
+import "../globals.css";
+
+const Map = () => {
+  return (
+    <>
+      <Sidebar />
+      <div className="text-center">Map</div>;
+    </>
+  );
+};
+
+export default Map;
---
+++ src/app/schedules/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../globals.css";
+import Sidebar from "@/components/sidebar";
+
+const Schedules = () => {
+  return (
+    <>
+      <Sidebar />
+      <div className="text-center">Schedules</div>;
+    </>
+  );
+};
+
+export default Schedules;
