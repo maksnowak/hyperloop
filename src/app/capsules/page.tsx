25109bdeb4e234dbed6608bd5f824b119d1d1a23
Alicja <alicjakruszewicz3@gmail.com>
2024-05-08 15:43:33 +0200

    Add basic capsule page with mock functionality

--- src/app/capsules/page.tsx
+++ src/app/capsules/page.tsx
@@ -1,12 +1,24 @@
 import Sidebar from "@/components/sidebar";
 import React from "react";
 import "../globals.css";
+import { PrismaClient } from "@prisma/client";
+import Capsule from "@/components/capsule";
 
-const Capsules = () => {
+const prisma = new PrismaClient();
+
+const Capsules = async () => {
+  const capsules = await prisma.capsules.findMany();
   return (
     <>
-      <Sidebar />
       <div className="text-center">Capsules</div>
+      <Sidebar />
+      <div className="relative">
+        <div className="ml-64 static grid grid-cols-3 inset-y-0 right-0 gap-y-8">
+          {capsules.map((capsule) => {
+            return <Capsule key={capsule.capsule_id} {...capsule} />;
+          })}
+        </div>
+      </div>
     </>
   );
 };
