6054a382f5c201dfc23a3511566c85868b3586fe
Alicja <alicjakruszewicz3@gmail.com>
2024-05-11 17:35:39 +0200

    Add config page for depots

--- src/app/depots/page.tsx
+++ src/app/depots/page.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "../globals.css";
 import Sidebar from "@/components/sidebar";
-const Depots = () => {
+import { PrismaClient } from "@prisma/client";
+import Depot from "@/components/depot";
+
+const prisma = new PrismaClient();
+
+const Depots = async () => {
+  const depots = await prisma.depots.findMany();
+
   return (
     <>
-      <Sidebar />
       <div className="text-center">Depots</div>
+      <div className="relative">
+        <Sidebar />
+        <div className="ml-64 static grid grid-cols-3 inset-y-0 right-0 gap-y-8">
+          {depots.map((depot) => {
+            return <Depot key={depot.depot_id} {...depot} />;
+          })}
+        </div>
+      </div>
     </>
   );
 };
