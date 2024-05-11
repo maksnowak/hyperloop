64bcded03e702963e902a35b1de9f9a5cc9362ee
permito <penciller@disroot.org>
2024-05-11 21:27:28 +0200

    Created PrismaClient singleton

--- src/web/app/page.tsx
+++ src/web/app/page.tsx
@@ -1,13 +1,7 @@
-import { PrismaClient } from "@prisma/client";
 import "./globals.css";
 import Sidebar from "@/components/sidebar";
 
-const prisma = new PrismaClient();
-
 export default async function Home() {
-  // const trains = await prisma.trains.findMany();
-  const capsules = await prisma.capsules.findMany();
-
   return <Sidebar />;
 }
 
