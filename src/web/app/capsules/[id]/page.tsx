00869f4fdb59413105b52372ee8389d67a2d5ea0
Duszke333 <duszke333@gmail.com>
2024-05-14 17:33:55 +0200

    Button for starting and ending repairs added.

--- src/web/app/capsules/[id]/page.tsx
+++ src/web/app/capsules/[id]/page.tsx
@@ -3,9 +3,10 @@ import "../../globals.css";
 import Sidebar from "@/components/sidebar";
 import Repair from "@/components/repair";
 import prisma from "../../client";
+import RepairActionButton from "@/app/capsules/[id]/repairActionButton";
 
 const CapsulePage = async ({ params }: { params: { id: string } }) => {
-  const repairs = await prisma.repairs_history.findMany({
+    var repairs = await prisma.repairs_history.findMany({
     where: {
       referred_capsule_id: Number(params.id),
     },
@@ -15,12 +16,20 @@ const CapsulePage = async ({ params }: { params: { id: string } }) => {
       where: { depot_id: r.performing_depot_id },
     }))
   );
+  const capsule = await prisma.capsules.findFirst({
+    where: { capsule_id: Number(params.id) },
+  });
 
   return (
     <div>
       <Sidebar />
-      <h1 className="text-center bold">Capsule number {params.id}</h1>
-        {repairs.map((r, i) => {
+      <h1 className="text-center bold">Capsule no. {params.id} repairs</h1>
+        <h3 className="text-center bold">Current status: {capsule.status}</h3>
+        <RepairActionButton status={capsule.status} capsule_id={capsule.capsule_id} />
+        <br></br>
+        <br></br>
+        {repairs.sort((r1, r2) => r1.repair_id - r2.repair_id)
+            .map((r, i) => {
           return <Repair repair={r} depot={depots[i]} />;
         })}
     </div>
