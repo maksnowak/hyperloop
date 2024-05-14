75b7715195ee36b4aa5ef0ba718952159117ea1d
permito <penciller@disroot.org>
2024-05-14 22:05:46 +0200

    Schedule visualization

--- src/web/app/capsules/page.tsx
+++ src/web/app/capsules/page.tsx
@@ -5,21 +5,19 @@ import Capsule from "@/components/capsule";
 import prisma from "../client";
 
 const Capsules = async () => {
-  const capsules = (await prisma.capsules.findMany())
-    .sort((c1, c2) => c1.capsule_id - c2.capsule_id)
-    .map((g) => <Capsule key={g.capsule_id} {...g} />);
+    const capsules = (await prisma.capsules.findMany())
+        .sort((c1, c2) => c1.capsule_id - c2.capsule_id)
+        .map((g) => <Capsule key={g.capsule_id} {...g} />);
 
-  return (
-    <>
-      <h1 className="text-center">Capsules</h1>
-      <div className="relative">
-        <Sidebar />
-        <div className="hyperloop-grid">
-          {capsules}
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <h1 className="text-center">Capsules</h1>
+            <Sidebar />
+            <div className="hyperloop-grid">
+                {capsules}
+            </div>
+        </>
+    );
 };
 
 export default Capsules;
