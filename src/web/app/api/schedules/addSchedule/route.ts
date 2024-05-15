949125f6d5f6a5873853d5939df98f97d5ead5a3
permito <penciller@disroot.org>
2024-05-16 01:28:44 +0200

    created route for schedules

--- src/web/app/api/schedules/addSchedule/route.ts
+++ src/web/app/api/schedules/addSchedule/route.ts
@@ -4,13 +4,13 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
 
-    const departure_time: string = searchParams.get("departure_time")!;
-    const arrival_time: string = searchParams.get("departure_time")!;
+    const departureTime: string = searchParams.get("departure_time")!;
+    const arrivalTime: string = searchParams.get("departure_time")!;
     const capsule: number = searchParams.get("capsule")!;
     // current_station_id: number;
     // next_station_id: number;
     // previous_schedule_id: number | null;
-    if (!departure_time || !arrival_time || !capsule) {
+    if (!departureTime || !arrivalTime || !capsule) {
         return NextResponse.json({
             message: "Missing departure time, arrival time or capsule",
             status: 400
@@ -18,7 +18,7 @@ export async function GET(request: Request) {
     }
 
     try {
-        await prisma.$queryRaw`CALL add_schedule(${departure_time}, ${arrival_time}, CAST(${capsule} AS INTEGER))`;
+        await prisma.$queryRaw`CALL add_schedule(${departureTime}, ${arrivalTime}, CAST(${capsule} AS INTEGER))`;
         return NextResponse.json({ message: "Schedule added successfully", status: 200 });
     } catch (e) {
         const errorMessage = e.message.split('ERROR:').at(-1).trim().replace(/`/g, '');
