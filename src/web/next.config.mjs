7af6dc4fed4a3c993ad521af24ca3f8a22b34efb
grzegorzstatkiewicz <grzegorz.statkiewicz@gmail.com>
2024-05-16 21:18:58 +0200

    Fix api routes in container

--- src/web/next.config.mjs
+++ src/web/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'standalone',
+	experimental: {
+		outputFileTracingIncludes: {
+			'/api/*': ['./app/api/**/*'],
+		},
+	},
 };
 
 export default nextConfig;
