1ced36d175ceadfb24c46c735772b94f38d2b2f4
permito <penciller@disroot.org>
2024-05-16 18:32:53 +0200

    change timetable of certain schedule (checkbox no. 2 from #8)

---
+++ src/web/app/api/schedules/setTimetable/route.ts
@@ -0,0 +1,35 @@
+import prisma from "../../../client";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+
+    const scheduleId: number = Number(searchParams.get("schedule_id")!);
+    const departureTime: string = searchParams.get("newDepartureTime")!;
+    const arrivalTime: string = searchParams.get("newArrivalTime")!;
+
+    if (departureTime == "" && arrivalTime == "") {
+        return NextResponse.json({
+            message: "Cannot change the timetable with the same times",
+            status: 400
+        });
+    }
+
+    try {
+        if (departureTime != "") {
+            await prisma.$queryRaw`UPDATE schedule SET departure_time=CAST(${departureTime} AS TIME) WHERE schedule_id=${scheduleId}`;
+        }
+
+        if (arrivalTime != "") {
+            await prisma.$queryRaw`UPDATE schedule SET arrival_time=CAST(${arrivalTime} AS TIME) WHERE schedule_id=${scheduleId}`;
+        }
+
+        return NextResponse.json({
+            message: `Succesfully updated schedule no. ${scheduleId}`,
+            status: 200
+        });
+    } catch (e: any) {
+        console.log(e.message);
+        return NextResponse.json({ message: e.message, status: 500 });
+    }
+}
---
+++ src/web/app/schedules/[id]/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../globals.css";
+import Sidebar from "@/components/sidebar";
+import prisma from "../../client";
+import TimetableForm from "@/components/timetableForm";
+
+const SchedulePage = async ({ params }: { params: { id: string } }) => {
+    const schedule = await prisma.schedule.findFirst({
+        where: { schedule_id: Number(params.id) },
+    });
+
+    return (
+        <>
+            <Sidebar />
+            <h1 className="text-center bold">Schedule no. {params.id}</h1>
+            <TimetableForm schedule={schedule} />
+        </>
+    );
+};
+
+export default SchedulePage;
