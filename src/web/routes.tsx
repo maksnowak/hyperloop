3cc39c121f13d2c790d59fb1743837bf1b5be39d
grzegorzstatkiewicz <grzegorz.statkiewicz@gmail.com>
2024-05-20 00:56:01 +0200

    Add missing route

--- src/web/routes.tsx
+++ src/web/routes.tsx
@@ -1,4 +1,4 @@
-import { ArrowRightEndOnRectangleIcon, CalendarDaysIcon, HomeIcon, LifebuoyIcon, MapIcon, WrenchIcon } from "@heroicons/react/24/outline";
+import { ArrowRightEndOnRectangleIcon, CalendarDaysIcon, ChartBarSquareIcon, HomeIcon, LifebuoyIcon, MapIcon, WrenchIcon } from "@heroicons/react/24/outline";
 
 export interface Route {
 	path: string;
@@ -13,4 +13,5 @@ export const routes: Route[] = [
 	{ path: '/capsules', title: 'Capsules', icon: <LifebuoyIcon className='w-6 h-6' /> },
 	{ path: '/schedules', title: 'Schedules', icon: <CalendarDaysIcon className='w-6 h-6' /> },
 	{ path: '/repairs', title: 'Repair History', icon: <WrenchIcon className='w-6 h-6' /> },
+	{ path: '/reports', title: 'Reports', icon: <ChartBarSquareIcon className='w-6 h-6' />}
 ];
