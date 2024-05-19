5d1f4258184a0d33e491fa6f4d2cd04ad07bcd16
grzegorzstatkiewicz <grzegorz.statkiewicz@gmail.com>
2024-05-19 02:36:51 +0200

    Change to server side component

--- src/web/components/navigation/sidebar.tsx
+++ src/web/components/navigation/sidebar.tsx
@@ -1,27 +1,32 @@
-"use client";
-
 import { Button } from '@nextui-org/react';
-import { usePathname, useRouter } from 'next/navigation';
 import { routes } from '@/routes';
+import { headers } from 'next/headers';
+import Link from 'next/link';
 
 export const Sidebar = () => {
-    const router = useRouter();
-    const pathname = usePathname();
+	const heads = headers();
+	const pathname = (heads.get('x-next-pathname') || '/?').split('?')[0];
+
+	console.log('Pathname: ', pathname);
 
 	return (
 		<aside className='fixed top-0 z-10 ml-[-100%] flex h-screen w-full flex-col justify-center border-r bg-black px-6 pb-3 transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]'>
 			<div className='space-y-2 tracking-wide'>
 				{routes.map((route, index) => (
-					<Button
-						key={index}
-						className='w-full'
-						color='primary'
-						variant={pathname === route.path || (route.path !== '/' && pathname.startsWith(route.path)) ? 'shadow' : 'light'}
-						startContent={route.icon}
-						onClick={() => router.push(route.path)}
-					>
-						{route.title}
-					</Button>
+					<div key={index}>
+						<Link href={route.path} passHref>
+							<Button
+								className='w-full'
+								color='primary'
+								variant={
+									pathname === route.path || (route.path !== '/' && pathname.startsWith(route.path)) ? 'shadow' : 'light'
+								}
+								startContent={route.icon}
+							>
+								{route.title}
+							</Button>
+						</Link>
+					</div>
 				))}
 			</div>
 		</aside>
