518429efff29372aaa0a1bfe6e0ff3a3e25a49fa
grzegorzstatkiewicz <grzegorz.statkiewicz@gmail.com>
2024-05-19 02:17:17 +0200

    Refactor

--- src/web/components/navigation/sidebar.tsx
+++ src/web/components/navigation/sidebar.tsx
@@ -1,23 +1,8 @@
 "use client";
 
 import { Button } from '@nextui-org/react';
-import { ArrowRightEndOnRectangleIcon, CalendarDaysIcon, HomeIcon, LifebuoyIcon, MapIcon, WrenchIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter } from 'next/navigation';
-
-interface Route {
-    path: string;
-    title: string;
-    icon?: JSX.Element;
-}
-
-const routes: Route[] = [
-    { path: '/', title: 'Map', icon: <MapIcon className='w-6 h-6' /> },
-    { path: '/stations', title: 'Stations', icon: <ArrowRightEndOnRectangleIcon className='w-6 h-6' /> },
-    { path: '/depots', title: 'Depots', icon: <HomeIcon className='w-6 h-6' /> },
-    { path: '/capsules', title: 'Capsules', icon: <LifebuoyIcon className='w-6 h-6' /> },
-    { path: '/schedules', title: 'Schedules', icon: <CalendarDaysIcon className='w-6 h-6' /> },
-    { path: '/repairs', title: 'Repair History', icon: <WrenchIcon className='w-6 h-6' /> },
-];
+import { routes } from '@/routes';
 
 export const Sidebar = () => {
     const router = useRouter();
