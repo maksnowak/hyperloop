fa4e251f17360d4eb7ffaeadc7126d8917176a5a
maksnowak <59841779+maksnowak@users.noreply.github.com>
2024-05-20 14:07:32 +0200

    Change tables to NextUI components

--- src/web/components/tubeTrips.tsx
+++ src/web/components/tubeTrips.tsx
@@ -1,3 +1,4 @@
+import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
 import React from "react";
 
 const getTableContent = (data: any) => {
@@ -14,12 +15,12 @@ const getTableContent = (data: any) => {
         minutes %= 60;
         // end of conversion
         rows.push(
-            <tr key={data[i].ride_id}>
-                <td>{start_date.toLocaleString("pl-PL", {timeZone: "UTC"})}</td>
-                <td>{end_date.toLocaleString("pl-PL", {timeZone: "UTC"})}</td>
-                <td>{data[i].capsules.model} (ID: {data[i].capsules.capsule_id})</td>
-                <td>{hours !== 0 ? hours+"h " : ""}{minutes}m {seconds}s</td>
-            </tr>
+            <TableRow key={data[i].ride_id}>
+                <TableCell>{start_date.toLocaleString("pl-PL", {timeZone: "UTC"})}</TableCell>
+                <TableCell>{end_date.toLocaleString("pl-PL", {timeZone: "UTC"})}</TableCell>
+                <TableCell>{data[i].capsules.model} (ID: {data[i].capsules.capsule_id})</TableCell>
+                <TableCell>{hours !== 0 ? hours+"h " : ""}{minutes}m {seconds}s</TableCell>
+            </TableRow>
         );
     }
     return rows;
@@ -47,19 +48,17 @@ const TubeTrips = async ({
     return (
         <div>
             <h3>Trips history</h3>
-            <table>
-                <thead>
-                    <tr key="head">
-                        <th>Entered at</th>
-                        <th>Left at</th>
-                        <th>Capsule</th>
-                        <th>Duration</th>
-                    </tr>
-                </thead>
-                <tbody>
+            <Table>
+                <TableHeader>
+                    <TableColumn>Entered at</TableColumn>
+                    <TableColumn>Left at</TableColumn>
+                    <TableColumn>Capsule</TableColumn>
+                    <TableColumn>Duration</TableColumn>
+                </TableHeader>
+                <TableBody>
                     {tableContent}
-                </tbody>
-            </table>
+                </TableBody>
+            </Table>
         </div>
     )
 };
