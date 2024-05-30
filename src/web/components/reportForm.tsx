04fd6506427d43ee43e28c76ae5e621a6d23ea55
maksnowak <59841779+maksnowak@users.noreply.github.com>
2024-05-30 14:19:54 +0200

    Add time to NextUI report form

--- src/web/components/reportForm.tsx
+++ src/web/components/reportForm.tsx
@@ -2,38 +2,38 @@
 import React, { use, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { DateRangePicker, Button, Select, SelectSection, SelectItem } from "@nextui-org/react";
-import { parseDate } from "@internationalized/date";
+import { parseDateTime } from "@internationalized/date";
 
 const ReportForm = () => {
     const [reportType, setReportType] = React.useState("Select report type");
     const [reportTarget, setReportTarget] = React.useState("");
     const [targets, setTargets] = React.useState(["Select report type first"])
     const [targetIDs, setTargetIDs] = React.useState([0] as number[]);
-    const [reportFrom, setReportFrom] = React.useState(new Date().toISOString().substring(0, 10));
-    const [reportTo, setReportTo] = React.useState(new Date().toISOString().substring(0, 10));
+    const [reportFrom, setReportFrom] = React.useState(new Date().toISOString().substring(0, 16));
+    const [reportTo, setReportTo] = React.useState(new Date().toISOString().substring(0, 16));
     const [reportRange, setReportRange] = React.useState({
-        start: parseDate(new Date().toISOString().substring(0, 10)),
-        end: parseDate(new Date().toISOString().substring(0, 10))
+        start: parseDateTime(new Date().toISOString().substring(0, 16)),
+        end: parseDateTime(new Date().toISOString().substring(0, 16))
     });
 
     const router = useRouter();
 
     React.useEffect(() => {
-        if (reportType === "Station") {
+        if (reportType === "station") {
             fetch(`/api/stations/getStations`).then((response) => response.json()).then((data) => {
                 let stationNames = data.data.map((station: any) => station.name);
                 let stationIDs = data.data.map((station: any) => station.station_id);
                 setTargets(stationNames);
                 setTargetIDs(stationIDs);
             });
-        } else if (reportType === "Tube") {
+        } else if (reportType === "tube") {
             fetch(`/api/tubes/getTubes`).then((response) => response.json()).then((data) => {
                 let tubeNames = data.data.map((tube: any) => tube.name);
                 let tubeIDs = data.data.map((tube: any) => tube.tube_id);
                 setTargets(tubeNames);
                 setTargetIDs(tubeIDs);
             });
-        } else if (reportType === "Capsule") {
+        } else if (reportType === "capsule") {
             fetch(`/api/capsules/getAllCapsules`).then((response) => response.json()).then((data) => {
                 let capsuleNames = data.data.map((capsule: any) => capsule.producer+" "+capsule.model+" (ID: "+capsule.capsule_id+")");
                 let capsuleIDs = data.data.map((capsule: any) => capsule.capsule_id);
@@ -75,7 +75,8 @@ const ReportForm = () => {
                         {targets.map((target) => <SelectItem key={target} value={target}>{target}</SelectItem>)}
                     </SelectSection>
                 </Select>
-                <DateRangePicker label="Report range" value={reportRange} onChange={(e) => {
+                {/* FIXME: Data końcowa nie działa do końca poprawnie, jakby cały ostatni dzień nie był brany pod uwagę */}
+                <DateRangePicker label="Report range" hideTimeZone value={reportRange} onChange={(e) => {
                     setReportRange({
                         start: e.start,
                         end: e.end
